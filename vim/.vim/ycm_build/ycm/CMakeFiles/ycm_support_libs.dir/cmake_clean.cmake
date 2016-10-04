b41af304c6a6d1d59ea4e5278afc0d028e910a4e
Geoffrey van Wyk <geoffrey@vanwyk.biz>
2016-10-05 01:47:36 +0200

    Removes bash and vim.

--- vim/.vim/ycm_build/ycm/CMakeFiles/ycm_support_libs.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-FILE(REMOVE_RECURSE
-  "CMakeFiles/ycm_support_libs"
-)
-
-# Per-language clean rules from dependency scanning.
-FOREACH(lang)
-  INCLUDE(CMakeFiles/ycm_support_libs.dir/cmake_clean_${lang}.cmake OPTIONAL)
-ENDFOREACH(lang)
